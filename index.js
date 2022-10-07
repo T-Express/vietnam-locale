766cd700d0f41ddad02ee37fa2dde4f9b77b7390
lan <lan.ng.1991@gmail.com>
2022-10-06 18:07:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const districts = require("./data/district.json");
+const wards = require("./data/ward.json");
+const provinces = require("./data/province.json");
+
+const keyBy = require("lodash.keyby");
+const groupBy = require("lodash.groupBy");
+
+const { matchSorter } = require("match-sorter");
+const removeAccents = require("remove-accents");
+
+const searchOptions = {
+	keys: ["slug_name"],
+	threshold: matchSorter.rankings.CONTAINS,
+};
+
+class VietnamLocale {
+	constructor() {
+		// === Province operation ====
+		this.provinceByCode = keyBy(provinces, "code");
+
+		// === District operation ====
+		this.districtsByProvincecode = groupBy(districts, "province_code");
+		this.districtByCode = keyBy(districts, "code");
+
+		// === Ward operation ====
+		this.wardsByProvinceAndDistrict = groupBy(wards, (item) => {
+			return `${item.province_code}::${item.district_code}`;
+		});
+		this.wardsByDistrict = groupBy(wards, "district_code");
+		this.wardsByProvince = groupBy(wards, "province_code");
+	}
+
+	search({ category, searchString, drillDown }) {
+		if (category === "province") {
+			return matchSorter(provinces, searchString, searchOptions);
+		} else if (category === "district") {
+			if (drillDown.provinceCode) {
+				return matchSorter(
+					this.districtsByProvincecode[drillDown.provinceCode],
+					searchString,
+					searchOptions
+				);
+			}
+
+			return matchSorter(districts, searchString, searchOptions);
+		} else if (category === "ward") {
+			if (drillDown.provinceCode && drillDown.districtCode) {
+				return matchSorter(
+					this.wardsByProvinceAndDistrict[
+						`${drillDown.provinceCode}::${drillDown.districtCode}`
+					],
+					searchString,
+					searchOptions
+				);
+			} else if (drillDown.provinceCode) {
+				return matchSorter(
+					this.wardsByProvince[drillDown.provinceCode],
+					searchString,
+					searchOptions
+				);
+			} else if (drillDown.districtCode) {
+				return matchSorter(
+					this.wardsByDistrict[drillDown.districtCode],
+					searchString,
+					searchOptions
+				);
+			}
+
+			return matchSorter(wards, searchString, searchOptions);
+		}
+	}
+
+	searchProvince(searchString) {
+		const rawString = removeAccents(searchString);
+
+		return this.search({
+			category: "province",
+			searchString: rawString,
+		});
+	}
+
+	searchDistrict(searchString, provinceCode = "") {
+		const rawString = removeAccents(searchString);
+
+		return this.search({
+			category: "district",
+			searchString: rawString,
+			drillDown: {
+				provinceCode,
+			},
+		});
+	}
+
+	searchWard(searchString, districtCode = "", provinceCode = "") {
+		const rawString = removeAccents(searchString);
+
+		return this.search({
+			category: "ward",
+			searchString: rawString,
+			drillDown: {
+				districtCode,
+				provinceCode,
+			},
+		});
+	}
+}
+
+module.exports = {
+	VietnamLocale,
+};
---
+++ test/basic_test.js
@@ -0,0 +1,96 @@
+const { VietnamLocale } = require("../index");
+const vnLocale = new VietnamLocale();
+const { expect } = require("chai");
+const provinces = require("../data/province.json");
+
+let hcm = provinces.find((p) => {
+	return p.slug_name === "Ho Chi Minh";
+});
+let q3 = "Quan 3";
+
+describe("Test Basic Functionality", () => {
+	describe("province", () => {
+		it("key by code", () => {
+			expect(vnLocale.provinceByCode[hcm.code]).to.be.an("object");
+			expect(vnLocale.provinceByCode[hcm.code].name).to.be.eq("Hồ Chí Minh");
+		});
+
+		it("group by code", () => {
+			const districtInHcm = vnLocale.districtsByProvincecode[hcm.code];
+			expect(districtInHcm).to.be.an("array");
+			expect(districtInHcm.find((d) => d.slug_name === q3)).to.be.an("object");
+		});
+	});
+
+	describe("ward groupby district and province", () => {
+		it("join key grouping", () => {
+			const districtInHcm = vnLocale.districtsByProvincecode[hcm.code];
+			const district3 = districtInHcm.find((d) => d.slug_name === q3);
+
+			const wardsInDistrict3 =
+				vnLocale.wardsByProvinceAndDistrict[`${hcm.code}::${district3.code}`];
+
+			expect(wardsInDistrict3).to.be.an("array");
+			const p5 = wardsInDistrict3.find((w) => {
+				return w.slug_name === "Phuong 5";
+			});
+
+			expect(p5).to.be.an("object");
+			expect(p5.name).to.be.eq("Phường 5");
+		});
+	});
+
+	describe("Search", () => {
+		it("search province", () => {
+			const shouldIncludeHcm = vnLocale.searchProvince("ho");
+			const hcmCity = shouldIncludeHcm.find((item) => {
+				return item.slug_name === hcm.slug_name;
+			});
+			expect(hcmCity).to.be.an("object");
+			expect(hcmCity.code).to.be.eq(hcm.code);
+		});
+
+		it("search district", () => {
+			const shouldIncludeHcmAsProvince = vnLocale.searchDistrict(q3);
+			const quan3 = shouldIncludeHcmAsProvince.find((item) => {
+				return item.province_code === hcm.code;
+			});
+
+			expect(quan3).to.be.an("object");
+		});
+
+		it("search district with drill_down", () => {
+			const moreProvince = vnLocale.searchDistrict("quan");
+			const lessProvince = vnLocale.searchDistrict("quan", hcm.code);
+
+			const quan3 = moreProvince.find((item) => {
+				return item.province_code === hcm.code;
+			});
+
+			const quan3_compare = lessProvince.find((item) => {
+				return item.province_code === hcm.code;
+			});
+
+			expect(moreProvince.length).to.be.gt(lessProvince.length);
+			expect(quan3).to.be.deep.equal(quan3_compare);
+		});
+
+		it("search ward with drill_down", () => {
+			const quan3 = vnLocale.districtsByProvincecode[hcm.code].find((item) => {
+				return item.slug_name === "Quan 3";
+			});
+
+			const allWards = vnLocale.searchWard("phuong 3");
+			const allWardsByHcm = vnLocale.searchWard("phuong 3", quan3.code);
+			const allWardsByHcmQ3 = vnLocale.searchWard(
+				"phuong 3",
+				quan3.code,
+				hcm.code
+			);
+
+			expect(allWards.length).to.be.gt(1); // ward can be repeated in Vietnam locale
+			expect(allWards.length).to.be.gt(allWardsByHcm.length);
+			expect(allWardsByHcm.length).to.be.gte(allWardsByHcmQ3.length);
+		});
+	});
+});
